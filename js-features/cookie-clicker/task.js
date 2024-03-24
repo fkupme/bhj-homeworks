1aa614b8519095ca341ae883cc4c5837ef7fd414
V. Kirichenko <fkupme@yandex.ru>
2024-03-24 17:17:21 +0300

    1 HW added

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,10 +1,19 @@
 const cookieClicker = () =>{
 	const cookie = document.getElementById('cookie');
 	const counter = document.getElementById('clicker__counter');
-
+	const speed = document.getElementById('clicker__speed');
+	let previous = 0
+	let current = 0
 	cookie.onclick = () =>{
 		counter.textContent ++;
 		cookie.setAttribute('width', '230px')
+
+		previous = current
+		current = new Date().getTime();
+		if (current !== previous){
+			speed.textContent = (1000 / (current - previous)).toFixed(2)
+		}
+		
 		setTimeout(() =>{
 			cookie.setAttribute('width', '200px')
 		}, 100);
