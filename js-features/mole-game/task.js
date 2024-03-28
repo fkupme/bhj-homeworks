ba19ab8fa4a42f5defc5edb67a88353dfb2d2b54
V. Kirichenko <fkupme@yandex.ru>
2024-03-28 21:44:35 +0300

    1 HW added

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,19 +1,33 @@
 const moleGame = () => {
 	const strikes = document.getElementById('dead');
 	const misses = document.getElementById('lost');
+	const victoryMessage = document.querySelector('.message-victory');
+	const defeatMessage = document.querySelector('.message-defeat');
 
-	getHole = i => document.getElementById(`hole${i}`)
+	const getHole = i => document.getElementById(`hole${i}`)
 
-	for (let i = 1; i < 9; i++) {
+	for (let i = 1; i <= 9; i++) {
 		const hole = getHole(i)
 		hole.onclick = () => {
 			if (hole.classList.contains('hole_has-mole')) {
 				strikes.textContent++;
-			} else { misses.textContent++; }
+				if (strikes.textContent >= 10) {
+					victoryMessage.classList.add('visible');
+					victoryMessage.onclick = () => {
+						location.reload();
+					}
+				}
+			} else {
+				misses.textContent++;
+				if (misses.textContent >= 5) {
+					defeatMessage.classList.add('visible');
+					defeatMessage.onclick = () => {
+						location.reload();
+					}
+				}
+			}
 		}
-
 	}
-
 };
 
 moleGame();
